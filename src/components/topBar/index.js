297c8dcd7382240c4ebc04c01f2af8f4a118d509
hylls <751448937@qq.com>
2020-09-02 17:51:40 +0800

    fist1

--- src/components/topBar/index.js
+++ src/components/topBar/index.js
@@ -4,8 +4,13 @@ export default memo(function TopBar() {
   return (
     <TopBarCss>
       <div className="topBar">
-        <div></div>
+        <div className="topBar_main">
+          <div className="logon">logo</div>
+          <div className="top-nav"></div>
+          <div className="top-search"></div>
+        </div>
       </div>
+      <div className="slide"></div>
     </TopBarCss>
   )
 })
--- src/components/topBar/style.js
+++ src/components/topBar/style.js
@@ -2,5 +2,24 @@ import styled from 'styled-components'
 
 export const TopBarCss = styled.div`
   height: 105px;
-  background-color: red;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  .topBar {
+    height: 70px;
+    width: 100%;
+    &_main {
+      height: 100%;
+      background-color: #242424;
+      .top-nav {
+        width: 508px;
+      }
+    }
+  }
+  .slide {
+    width: 100%;
+    background-color: #C20C0C;
+    height: 35px;
+  }
+  
 `
